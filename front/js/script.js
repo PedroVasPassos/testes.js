fbfb8f35e11a94a7f05ce3c3243ac244ec702180
PedroVasPassos <pdvasconcelos01@gmail.com>
2024-10-09 09:21:11 -0300

    2.12 finalizada

--- front/js/script.js
+++ front/js/script.js
@@ -2,9 +2,17 @@ const protocolo = 'http://'
 const baseURL = 'localhost:3000'
 const filmeEndpoint = '/filmes'
 
- async function obterFilme () {
+ async function obterFilmes () {
     const URLcompleta = `${protocolo}${baseURL}${filmeEndpoint}`
     const filmes = (await axios.get(URLcompleta)).data 
-
+    let tabela = document.querySelector('.filmes')
+    let corpoTabela = tabela.getElementsByTagName('tbody')[0]
+    for (let filme of filmes) {
+        let linha = corpoTabela.insertRow(0)
+        let celulaTitulo = linha.insertCell(0)
+        let celulaSinopse = linha.insertCell(1)
+        celulaTitulo.innerHTML = filme.titulo
+        celulaSinopse.innerHTML = filme.sinopse
+    }
     console.log (filmes)
 }
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require ("express")
+const cors = require ('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 //get http://localhost:3000/oi
-app.get("/oi", (req, res) => 
-{res.send("oi")
+app.get("/oi", (req, res) => {
+    res.send("oi")
 })
 
 let filmes = [
