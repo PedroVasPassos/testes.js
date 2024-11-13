d88aeb693a095eaf0053ccd5ccbb79ff7cd1643c
PEDRO VASCONCELOS PASSOS <24.00923-7@maua.br>
2024-11-13 09:59:38 -0300

    finalizei a parte de login, mas esta dando um erro

--- backend.js
+++ backend.js
@@ -1,38 +1,36 @@
-
-const express = require("express")
-const cors = require('cors')
+const express = require ('express')
+const cors = require ('cors')
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const uniqueValidator = require('mongoose-unique-validator')
 const app = express()
 
-app.use(express.json())
-app.use(cors())
+app.use (express.json())
+app.use (cors())
 
 const Filme = mongoose.model("Filme", mongoose.Schema({
-    titulo: { type: String },
-    sinopse: { type: String }
+    titulo: {type: String},
+    sinopse: {type: String}
 }))
 
 const usuarioSchema = mongoose.Schema ({
     login: {type: String, required: true, unique: true},
     password: {type: String, required: true}
 })
-
 usuarioSchema.plugin(uniqueValidator)
 const Usuario = mongoose.model("Usuario", usuarioSchema)
 
-async function conectarAoMongo() {
+async function conectarAoMongo () {
     await mongoose.connect(`mongodb+srv://pedro:123_456@cluster0.vixpx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+    
 }
-
 app.get("/filmes", async (req, res) => {
     const filmes = await Filme.find()
-    res.json(filmes) })
-
+    res.json(filmes)
+})
 app.post("/filmes", async (req, res) => {
-    //captura o que o usuario enviou
+    //captura o que o usuário enviou
     const titulo = req.body.titulo
     const sinopse = req.body.sinopse
     //montar um objeto de acordo com o modelo Filme
@@ -41,7 +39,6 @@ app.post("/filmes", async (req, res) => {
     const filmes = await Filme.find()
     res.json(filmes)
 })
-
 app.post("/signup", async (req, res) => {
     try {
         const login = req.body.login
@@ -80,10 +77,9 @@ app.post('/login', async (req, res) => {
 app.listen(3000, () => {
     try {
         conectarAoMongo()
-        console.log("server up and running e conexão ok");
+        console.log("server up and running e conexão ok")
     }
     catch (e) {
-    console.log('erro na conexão', e)
-    }}
-)
-
+        console.log ('erro na conexão', e)
+    }
+})
--- front/js/frontend.js
+++ front/js/frontend.js
@@ -9,12 +9,12 @@ function exibeAlerta (seletor, innerHTML, classesToAdd, classesToRemove, timeout
     setTimeout(() => {
         alert.classList.remove(...classesToAdd)
         alert.classList.add (...classesToRemove)
-    }, timeout)
+    }, timeout)  
 }
 function escondeModal (idModal, timeout) {
     setTimeout (() => {
-    let modalCadastro = bootstrap.Modal.getInstance(document.querySelector(idModal))
-    modal.hide()
+        let modal = bootstrap.Modal.getInstance(document.querySelector(idModal))
+        modal.hide()
     }, timeout)
 }
 
@@ -30,7 +30,6 @@ function listarFilmes (filmes) {
         celulaSinopse.innerHTML = filme.sinopse
     }
 }
-
 async function obterFilmes () {
     const filmesEndpoint = '/filmes'
     const URLcompleta = `${protocolo}${baseURL}${filmesEndpoint}`
@@ -51,37 +50,72 @@ async function cadastrarFilme() {
         listarFilmes(filmes)
     }
     else {
-        exibeAlerta('.alert-filme', "Preencha todos os campos!!!", ['alert-danger', 'show'], ['d-none'], 3000)
+        exibeAlerta('.alert-filme', "Preencha todos os campos!!", ['alert-danger', 'show'], 
+                   ['d-none', 'alert-success'], 2000)
     }
 } 
-async function cadastrarUsuario() {
+async function cadastrarUsuario() { 
     let usuarioCadastroInput = document.querySelector('#usuarioCadastroInput')
     let passwordCadastroInput = document.querySelector('#passwordCadastroInput')
     let usuarioCadastro = usuarioCadastroInput.value
     let passwordCadastro = passwordCadastroInput.value
-    if (usuarioCadastro && passwordCadastro) {
+    if (usuarioCadastro && passwordCadastro){
         try {
-            const cadastroEndpoit = '/signup'
-            const URLcompleta = `${protocolo}${baseURL}${cadastroEndpoit}`
-            await axios.post(URLcompleta, {login: usuarioCadastro, password: passwordCadastro})
-
+            const cadastroEndpoint = '/signup'
+            const URLcompleta = `${protocolo}${baseURL}${cadastroEndpoint}`
+            await axios.post(
+                URLcompleta,
+                {login: usuarioCadastro, password: passwordCadastro}
+            )
             usuarioCadastroInput.value = ""
             passwordCadastroInput.value = ""
-
-            exibeAlerta('.alert-modal-cadastro', "Usuário cadastrado com sucesso!!!", ['show', 'alert-success'], ['d-none'], 3000)
-
-            escondeModal('#modalCadastro', 3000)
-
+            exibeAlerta('.alert-modal-cadastro', "Usuário cadastrado com sucesso!!!", ['show', 'alert-success'], ['d-none', 'alert-danger'], 2000)
+            escondeModal('#modalCadastro', 2000)
         }
         catch (e) {
-
-            exibeAlerta('.alert-modal-cadastro', "Não foi possivel realizar o cadastro!!!", ['show', 'alert-danger'], ['d-none'], 3000)
-
-            escondeModal('#modalCadastro', 3000)
+            exibeAlerta('.alert-modal-cadastro', "Não foi possível realizar o cadastro!!!", ['show', 'alert-danger'], ['d-none', 'alert-success'], 2000)
+            escondeModal('#modalCadastro', 2000)
         }
     }
     else {
-        exibeAlerta('.alert-modal-cadastro', "Preencha todos os campos!!!", ['show', 'alert-danger'], ['d-none'], 3000)
+        exibeAlerta('.alert-modal-cadastro', "Preencha todos os campos!!!", ['show', 'alert-danger'], ['d-none', 'alert-success'], 2000)
     }
-
+}
+const fazerLogin = async () => {
+    let usuarioLoginInput = document.querySelector('#usuarioLoginInput')
+    let passwordLoginInput = document.querySelector('#passwordLoginInput')
+    usuarioLogin = usuarioLoginInput.value
+    passwordLogin = passwordLoginInput.value
+    if (usuarioLogin && passwordLogin) {
+        try {
+            const loginEndpoint = '/login'
+            const URLcompleta = `${protocolo}${baseURL}${loginEndpoint}`
+            const response = await axios.post(
+                URLcompleta,
+                {login: usuarioLogin, password: passwordLogin}
+            )
+            //console.log(response.data)
+            localStorage.setItem("token", response.data)
+            usuarioLoginInput.value = ""
+            passwordLoginInput.value = ""
+            exibeAlerta('.alert-modal-login', "Login realizado com sucesso!!!", 
+                        ['show', 'alert-success'], ['d-none', 'alert-danger'], 2000)
+            escondeModal('#modalLogin', 2000)
+            const loginLink = document.querySelector('#loginLink')
+            loginLink.innerHTML = "Logout"
+            const cadastrarFilmeButton = document.querySelector("#cadastrarFilmeButton")
+            cadastrarFilmeButton.disabled = false
+        }
+        catch (e) {
+            exibeAlerta('.alert-modal-login', 'Falha na autenticação', ['show', 'alert-danger'],
+                        ['d-none', 'alert-success'], 2000
+            )
+            escondeModal('#modalLogin', 2000)
+        }
+    }
+    else {
+        exibeAlerta('.alert-modal-login', 'Preencha todos os campos!!!', ['show', 'alert-danger'],
+                    ['d-none', 'alert-success'], 2000
+        )
+    }
 }
